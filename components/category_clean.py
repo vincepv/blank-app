d40184914c71aa32e20886e9b33bca9f91d512a9
vpv <support@digitalebox.fr>
2025-01-09 18:01:54 +0100

    use constant in catergoy

--- components/category_clean.py
+++ components/category_clean.py
@@ -1,3 +1,5 @@
+from constant.column_name import CATEGORIE
+
 def category_clean(df):
     """
     Reassigns values in the 'Categorie' column of a DataFrame.
@@ -12,6 +14,7 @@ def category_clean(df):
         '\.0$': '', 
     }
     category_mapping = {2: 1, 3: 2, 4: 3, 5: 4, 6: 4, 7: 4, 8: 4, 9: 4}
-    df['Categorie'] = df['Categorie'].map(category_mapping)
-    df['Categorie'] = df['Categorie'].astype(str).replace(dic_category, regex=True)
+
+    df[CATEGORIE] = df[CATEGORIE].map(category_mapping)
+    df[CATEGORIE] = df[CATEGORIE].astype(str).replace(dic_category, regex=True)
     return df
