b1fa8b59b445d0c340450b8438c2968b944450cf
vpv <support@digitalebox.fr>
2025-01-10 12:23:12 +0100

    fix date component

--- components/date_clean.py
+++ components/date_clean.py
@@ -4,7 +4,6 @@ from constant.column_name import DATE_NAISSANCE
 def date_clean (df):
 
   #clean false date format
-  
   if DATE_NAISSANCE not in df:
     df.insert(loc=0, column=DATE_NAISSANCE,value = '')
 
@@ -12,12 +11,11 @@ def date_clean (df):
     '^00':'01',
     '/00/':'/01/',
   }
-
-  df['clean_date'] = df[DATE_NAISSANCE].replace(clean_value,regex=True)
+  df[DATE_NAISSANCE] = df[DATE_NAISSANCE].replace(clean_value,regex=True)
 
   # convert in: yyyy-mm-dd
-  df['clean_date'] = pd.to_datetime(
-    df['clean_date'],
+  df[DATE_NAISSANCE] = pd.to_datetime(
+    df[DATE_NAISSANCE],
     errors='ignore',
     dayfirst=True,)
 
